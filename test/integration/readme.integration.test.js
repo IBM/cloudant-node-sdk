95c2a046d8f74eed43d23bcd1ea04e9b43f3cfe0
Eigel Ildikó <ildiko.eigel@ibm.com>
2022-02-16 08:40:41 +0100

    ci(dev-deps): remove axios (#710)

--- test/integration/readme.integration.test.js
+++ test/integration/readme.integration.test.js
@@ -1,5 +1,5 @@
 /**
- * © Copyright IBM Corporation 2020. All Rights Reserved.
+ * © Copyright IBM Corporation 2020, 2022. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const axios = require('axios');
+const http = require('http');
 const fs = require('fs');
 const sinon = require('sinon');
 const {
@@ -22,8 +22,6 @@ const {
 const { createDbAndDoc } = require('../examples/src/js/CreateDbAndDoc.js');
 const { updateDoc } = require('../examples/src/js/UpdateDoc.js');
 const { deleteDoc } = require('../examples/src/js/DeleteDoc.js');
-
-axios.defaults.adapter = require('axios/lib/adapters/http'); // workaround from https://stackoverflow.com/a/57320262
 
 const setAuthentication = () => {
   process.env.CLOUDANT_AUTH_TYPE = 'basic';
@@ -39,9 +37,26 @@ describe('Readme integration tests', () => {
   let consoleLogSpy;
 
   // jest --runInBand should be set to run the test cases in order
-  beforeAll(async () => {
-    await axios.post(`${process.env.WIREMOCK_URL}/__admin/scenarios/reset`); // Reset Wiremock states
+  beforeAll(() => {
+    const wiremockURL = new URL(process.env.WIREMOCK_URL);
+    const request = http.request({
+      host: wiremockURL.hostname,
+      port: wiremockURL.port,
+      path: '/__admin/scenarios/reset',
+      method: 'POST',
+    });
+    const p = new Promise((resolve, reject) => {
+      request
+        .on('response', (resp) => {
+          resolve(resp);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+    request.end();
     setAuthentication();
+    return p; // Reset Wiremock states
   });
 
   beforeEach(() => {
