5cfa8b927ff14609a147634cd800068a1f211ef1
Ildiko Eigel <ildiko.eigel@ibm.com>
2022-03-09 13:12:05 +0100

    docs: fix timestamps for README

--- test/examples/src/ts/CreateDbAndDoc.ts
+++ test/examples/src/ts/CreateDbAndDoc.ts
@@ -55,7 +55,7 @@ const exampleDocument: OrderDocument = { _id: exampleDocId };
 
 // Add "name" and "joined" fields to the document
 exampleDocument.name = 'Bob Smith';
-exampleDocument.joined = '2019-01-24T10:42:99.000Z';
+exampleDocument.joined = '2019-01-24T10:42:59.000Z';
 
 // Save the document in the database with "postDocument" function
 createDb.then(() => {
