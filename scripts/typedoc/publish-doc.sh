acde19a8f8ec48ef2852bb81c2fc7c9a6f60c6a8
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2023-08-30 14:25:25 +0000

    docs(generated): use npm run to build node's docs instead of external bash running npx
    Generated SDK source code using:
    - Generator version 3.76.0
    - Specification version 1.0.0-dev0.0.52
    - Automation (cloudant-sdks) version f976e4c
    
    Signed-off-by: cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>

--- scripts/typedoc/publish-doc.sh
+++ scripts/typedoc/publish-doc.sh
@@ -8,7 +8,7 @@ GIT_REPO=$(git remote get-url origin)
 
 # Create documentation
 printf ">>>>> Generate new documentation\n"
-./scripts/typedoc/generate_typedoc.sh
+npm run typedoc
 
 # Clone gh-pages branch
 printf ">>>>> Publishing typedoc for release build: repo=%s branch=%s build_num=%s job_name=%s\n" ${GIT_REPO} ${BRANCH_NAME} ${BUILD_NUMBER} ${JOB_NAME}
