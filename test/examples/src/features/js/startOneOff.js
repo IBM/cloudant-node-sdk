2fe304e4e0258f180fbe556d78c529ee2354a182
Esteban Laver <10552966+emlaver@users.noreply.github.com>
2024-01-10 18:26:45 -0500

    test: update Node changes follower src examples to ES style
    Signed-off-by: Esteban Laver <10552966+emlaver@users.noreply.github.com>

--- test/examples/src/features/js/startOneOff.js
+++ test/examples/src/features/js/startOneOff.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const { ChangesFollower, CloudantV1 } = require('../../../../../index');
+import { ChangesFollower, CloudantV1 } from '../../../../../index';
 
 const client = CloudantV1.newInstance();
 const changesParams = {
