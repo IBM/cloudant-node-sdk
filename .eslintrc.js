48c8ea2df9f9870a35482af6a4cafc8bccd8833b
Ildiko Eigel <ildiko.eigel@ibm.com>
2024-11-20 13:23:07 +0100

    build: remove eslint-config-airbnb-typescript
    And use eslint rules from eslint-config-airbnb-typescript on ts files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /**
- * © Copyright IBM Corporation 2020. All Rights Reserved.
+ * © Copyright IBM Corporation 2020, 2024. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const {
+  rules: baseBestPracticesRules,
+} = require('eslint-config-airbnb-base/rules/best-practices');
+const {
+  rules: baseErrorsRules,
+} = require('eslint-config-airbnb-base/rules/errors');
+const { rules: baseES6Rules } = require('eslint-config-airbnb-base/rules/es6');
+const {
+  rules: baseImportsRules,
+} = require('eslint-config-airbnb-base/rules/imports');
+const {
+  rules: baseStyleRules,
+} = require('eslint-config-airbnb-base/rules/style');
+const {
+  rules: baseVariablesRules,
+} = require('eslint-config-airbnb-base/rules/variables');
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 module.exports = {
   'root': true,
   'env': {
@@ -68,11 +89,7 @@ module.exports = {
     // TypeScript specific rules
     {
       'files': ['**/*.ts'],
-      'extends': [
-        'airbnb-typescript/base',
-        'plugin:import/typescript',
-        'prettier',
-      ],
+      'extends': ['plugin:import/typescript', 'prettier'],
       'parser': '@typescript-eslint/parser',
       'parserOptions': {
         'project': 'tsconfig.json',
@@ -85,8 +102,87 @@ module.exports = {
         'prettier',
       ],
       'rules': {
-        '@typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
+        'default-param-last': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/default-param-last':
+          baseBestPracticesRules['default-param-last'],
+
+        'dot-notation': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/dot-notation':
+          baseBestPracticesRules['dot-notation'],
+
+        'no-array-constructor': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-array-constructor':
+          baseStyleRules['no-array-constructor'],
+
+        'no-empty-function': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-empty-function':
+          baseBestPracticesRules['no-empty-function'],
+
+        'no-implied-eval': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-implied-eval':
+          baseBestPracticesRules['no-implied-eval'],
+
+        // @typescript-eslint/no-loss-of-precision is deprecated, but
+        // we still need error on no-loss-of-precision:
+        'no-loss-of-precision': baseErrorsRules['no-loss-of-precision'],
+
+        'no-loop-func': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-loop-func':
+          baseBestPracticesRules['no-loop-func'],
+
+        'no-shadow': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-shadow': baseVariablesRules['no-shadow'],
+
+        'no-throw-literal': 'off', // disable base rule in favour of the extended typescript rule:
+        // in @typescript-eslint no-throw-literal became only-throw-error:
+        '@typescript-eslint/only-throw-error':
+          baseBestPracticesRules['no-throw-literal'],
+
+        'no-unused-expressions': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-unused-expressions':
+          baseBestPracticesRules['no-unused-expressions'],
+
+        'no-use-before-define': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          {
+            'functions': false,
+            'classes': false,
+            'enums': false,
+            'typedefs': false,
+          },
+        ],
+
+        'no-useless-constructor': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/no-useless-constructor':
+          baseES6Rules['no-useless-constructor'],
+
+        'return-await': 'off', // disable base rule in favour of the extended typescript rule:
+        '@typescript-eslint/return-await': [
+          baseBestPracticesRules['no-return-await'],
+          'in-try-catch',
+        ],
+
+        'import/no-unresolved': 'error',
+
+        // Ensure consistent use of file extension within the import path
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+        // and append 'ts' and 'tsx' to Airbnb 'import/extensions' config
+        'import/extensions': [
+          baseImportsRules['import/extensions'][0],
+          baseImportsRules['import/extensions'][1],
+          {
+            ...baseImportsRules['import/extensions'][2],
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+
+        'import/no-extraneous-dependencies': [
+          'error',
+          { 'devDependencies': false, 'optionalDependencies': false },
+        ],
+
         'no-param-reassign': 'off',
         'no-underscore-dangle': 'off',
         'prettier/prettier': 'error',
