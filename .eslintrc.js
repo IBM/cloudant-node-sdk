c031171606bb258048c2e025a75f9df1853d8132
Ildiko Eigel <ildiko.eigel@ibm.com>
2025-07-28 12:08:22 +0200

    build: replace base best practices eslint rules
    see https://github.com/airbnb/javascript/blob/eslint-config-airbnb-base-v15.0.0/packages/eslint-config-airbnb-base/rules/best-practices.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /**
- * © Copyright IBM Corporation 2020, 2024. All Rights Reserved.
+ * © Copyright IBM Corporation 2020, 2025. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,6 @@
  */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-require-imports */
-const {
-  rules: baseBestPracticesRules,
-} = require('eslint-config-airbnb-base/rules/best-practices');
 const {
   rules: baseErrorsRules,
 } = require('eslint-config-airbnb-base/rules/errors');
@@ -103,44 +100,60 @@ module.exports = {
       ],
       'rules': {
         'default-param-last': 'off', // disable base rule in favour of the extended typescript rule:
-        '@typescript-eslint/default-param-last':
-          baseBestPracticesRules['default-param-last'],
+        // https://eslint.org/docs/rules/default-param-last
+        '@typescript-eslint/default-param-last': 'error',
 
         'dot-notation': 'off', // disable base rule in favour of the extended typescript rule:
-        '@typescript-eslint/dot-notation':
-          baseBestPracticesRules['dot-notation'],
+        '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
 
         'no-array-constructor': 'off', // disable base rule in favour of the extended typescript rule:
         '@typescript-eslint/no-array-constructor':
           baseStyleRules['no-array-constructor'],
 
         'no-empty-function': 'off', // disable base rule in favour of the extended typescript rule:
-        '@typescript-eslint/no-empty-function':
-          baseBestPracticesRules['no-empty-function'],
+        // disallow empty functions, except for standalone funcs/arrows
+        // https://eslint.org/docs/rules/no-empty-function
+        '@typescript-eslint/no-empty-function': [
+          'error',
+          {
+            allow: ['arrowFunctions', 'functions', 'methods'],
+          },
+        ],
 
         'no-implied-eval': 'off', // disable base rule in favour of the extended typescript rule:
-        '@typescript-eslint/no-implied-eval':
-          baseBestPracticesRules['no-implied-eval'],
+        // disallow use of eval()-like methods
+        // https://eslint.org/docs/rules/no-implied-eval
+        '@typescript-eslint/no-implied-eval': 'error',
 
         // @typescript-eslint/no-loss-of-precision is deprecated, but
         // we still need error on no-loss-of-precision:
         'no-loss-of-precision': baseErrorsRules['no-loss-of-precision'],
 
         'no-loop-func': 'off', // disable base rule in favour of the extended typescript rule:
-        '@typescript-eslint/no-loop-func':
-          baseBestPracticesRules['no-loop-func'],
+        // disallow creation of functions within loops
+        // https://eslint.org/docs/rules/no-loop-func
+        '@typescript-eslint/no-loop-func': 'error',
 
         'no-shadow': 'off', // disable base rule in favour of the extended typescript rule:
         '@typescript-eslint/no-shadow': baseVariablesRules['no-shadow'],
 
         'no-throw-literal': 'off', // disable base rule in favour of the extended typescript rule:
         // in @typescript-eslint no-throw-literal became only-throw-error:
-        '@typescript-eslint/only-throw-error':
-          baseBestPracticesRules['no-throw-literal'],
+        // restrict what can be thrown as an exception
+        // https://eslint.org/docs/rules/no-throw-literal
+        '@typescript-eslint/only-throw-error': 'error',
 
         'no-unused-expressions': 'off', // disable base rule in favour of the extended typescript rule:
-        '@typescript-eslint/no-unused-expressions':
-          baseBestPracticesRules['no-unused-expressions'],
+        // disallow usage of expressions in statement position
+        // https://eslint.org/docs/rules/no-unused-expressions
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: false,
+            allowTernary: false,
+            allowTaggedTemplates: false,
+          },
+        ],
 
         'no-use-before-define': 'off', // disable base rule in favour of the extended typescript rule:
         '@typescript-eslint/no-use-before-define': [
@@ -158,10 +171,9 @@ module.exports = {
           baseES6Rules['no-useless-constructor'],
 
         'return-await': 'off', // disable base rule in favour of the extended typescript rule:
-        '@typescript-eslint/return-await': [
-          baseBestPracticesRules['no-return-await'],
-          'in-try-catch',
-        ],
+        // disallow redundant `return await`
+        // https://eslint.org/docs/rules/no-return-await
+        '@typescript-eslint/return-await': ['error', 'in-try-catch'],
 
         'import/no-unresolved': 'error',
 
