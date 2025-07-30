a9a2b2c438c54ecf71dbd1051b6f0201a2feb467
Ildiko Eigel <ildiko.eigel@ibm.com>
2025-07-30 10:58:03 +0200

    build: enable the max-classes-per-file rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -203,7 +203,6 @@ module.exports = {
         'eqeqeq': ['error', 'always', { 'null': 'ignore' }],
         // Turn off not relevant airbnb rules on typescript files:
         'import/no-cycle': 'off',
-        'max-classes-per-file': 'off',
         'no-case-declarations': 'off',
         'no-dupe-class-members': 'off',
         'no-else-return': 'off',
