5fba10a649fdc30deba98665a9187d71374c96cd
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2025-10-22 09:09:34 +0000

    PR feedback(generated): use enum values instead of plain strings
    Generated SDK source code using:
    - Generator version 3.107.1
    - Specification version 1.0.0-dev0.1.29
    - Automation (cloudant-sdks) version 52aab7c

--- examples/snippets/postActivityTrackerEvents/example_request.js
+++ examples/snippets/postActivityTrackerEvents/example_request.js
@@ -4,7 +4,10 @@ import { CloudantV1 } from '@ibm-cloud/cloudant';
 const service = CloudantV1.newInstance({});
 
 service.postActivityTrackerEvents({
-  types: ['management'],
+  types: [
+    CloudantV1.ActivityTrackerEvents.Constants.Types.MANAGEMENT,
+    CloudantV1.ActivityTrackerEvents.Constants.Types.DATA
+  ],
 }).then(response => {
   console.log(response.result);
 });
